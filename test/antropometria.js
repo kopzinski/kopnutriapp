5d0238c939da1a51651f8ee37c3eac199ab8bbae
Paulo Eduardo Kopzinski Mello <eduardo.geleia@gmail.com>
2016-02-24 18:58:46 -0300

    inserindo antro

---
+++ test/antropometria.js
@@ -0,0 +1,35 @@
+var express = require('../config/express')();
+var request = require('supertest')(express);
+var assert = require('assert');
+describe('#AntropometriaController',function(){
+
+	// beforeEach(function(done){
+	// 	var conn = express.infra.connectionFactory();
+	// 	conn.query("delete from livros",function(ex, results){
+	// 		if(!ex) {
+	// 			done();
+	// 		}
+	// 	});
+	// });
+
+	it('#listagem json',function(done) {
+		request.get('/antropometrias/1')
+		.set('Accept','application/json')
+		.expect('Content-Type',/json/)
+		.expect(200,done);
+	});
+
+	// it('#cadastro novo produto com dados invalidos',function(done){
+	// 	request.post('/produtos')
+	// 	.send({titulo:"",descricao:"novo livro"})
+	// 	.expect(400,done);
+	// });
+
+	it('#registra nova antropometria com dados validos',function(done){
+		request.post('/antropometrias')
+		.send({idade:30,peso:91.0})
+		.expect(200,done);
+	});
+
+});
+	
